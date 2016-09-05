6911ae47933009f6adb9886225608444a5418f2c
Larry Salibra <larry@appartisan.com>
2016-09-05 17:02:29 +0800

    add name page with history

--- public/src/js/services/names.js
+++ public/src/js/services/names.js
@@ -10,6 +10,26 @@ angular.module('insight.names')
         method: 'GET',
         interceptor: {
           response: function (res) {
+
+            /*
+             * Convert history JSON object of block height keys & arrays of operations
+             * to one array of name operations
+             */
+            var nameops = [];
+            var history = res.data.history;
+            for (var key in history) {
+              if (history.hasOwnProperty(key)) {
+                  for(var i = 0; i < history[key].length; i++) {
+                    var nameop = history[key][i];
+                    nameop.blockHeight = parseInt(key);
+                    nameops.push(nameop);
+                  }
+              }
+            }
+
+            nameops.reverse();
+            res.data.history = nameops;
+
             return res.data;
           },
           responseError: function (res) {

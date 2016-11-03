e0855eab786147b4b77d54b172ac27073e6cd4ad
Larry Salibra <larry@appartisan.com>
2016-11-04 01:01:11 +0800

    add cache control headers & remove unused routes

--- tools/runserver.py
+++ tools/runserver.py
@@ -9,7 +9,7 @@ from flask_crossdomain import crossdomain
 
 from blockstack_client import client as bs_client
 import blockstack_client
-session = bs_client.session(server_host="localhost", server_port=6264, set_global=True)
+session = bs_client.session(server_host="node.blockstack.org", server_port=6264, set_global=True)
 
 app = Flask(__name__)
 
@@ -18,18 +18,10 @@ app = Flask(__name__)
 def get_all_namespaces():
     try:
         namespaces = session.get_all_namespaces()
-        return jsonify(namespaces), 200
+        return jsonify(namespaces), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
-@app.route('/get_name_blockchain_record/<fqu>', methods=['GET'])
-@crossdomain(origin='*')
-def get_name_blockchain_record(fqu):
-    try:
-        blockchain_record = bs_client.get_name_blockchain_record(fqu)
-        return jsonify(blockchain_record), 200
-    except Exception as e:
-        return jsonify(str(e)), 500
 
 @app.route('/get_name_blockchain_history/<fqu>', methods=['GET'])
 @crossdomain(origin='*')
@@ -41,7 +33,10 @@ def get_name_blockchain_history(fqu):
         start_block = 0
         end_block = height
         blockchain_history = bs_client.get_name_blockchain_history(fqu, start_block, end_block)
-        return jsonify(blockchain_history), 200
+        if blockchain_history == {}:
+            return jsonify({'error': 'Not found'}), 404, {'Cache-Control': 'public, max-age=300'}
+        else:
+            return jsonify(blockchain_history), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -57,7 +52,7 @@ def get_names_in_namespace(namespace, page_num):
         reply = {}
         reply['names'] = bs_client.get_names_in_namespace(namespace, offset, count)
         reply['total_pages'] = int(math.ceil(bs_client.get_num_names_in_namespace(namespace) / float(NAMES_PER_PAGE)))
-        return jsonify(reply), 200
+        return jsonify(reply), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -67,7 +62,7 @@ def get_names_owned_by_address(address):
     try:
         reply = {}
         reply['names'] = blockstack_client.profile.get_names_owned_by_address(address)
-        return jsonify(reply), 200
+        return jsonify(reply), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -76,7 +71,7 @@ def get_names_owned_by_address(address):
 def get_name_zonefile(fqu):
     try:
         zonefile = blockstack_client.profile.get_name_zonefile(fqu)
-        return jsonify(zonefile), 200
+        return jsonify(zonefile), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -85,8 +80,19 @@ def get_name_zonefile(fqu):
 def get_nameops_at(blocknum):
     try:
         nameops = {}
+        info = bs_client.getinfo()
+        height = int(info["last_block_processed"])
+
+        # block has yet to be processed by blockstack core
+        if height < int(blocknum):
+            return jsonify([]), 404, {'Cache-Control': 'public, max-age=10'}
+
+
         nameops["nameops"] = bs_client.get_nameops_at(int(blocknum))
-        return jsonify(nameops), 200
+
+        # get_nameops_at is very expensive for blocks with many ops
+        # and results never change (blockchains! yay!) so cache for 1 year
+        return jsonify(nameops), 200, {'Cache-Control': 'public, max-age=31536000'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -95,7 +101,10 @@ def get_nameops_at(blocknum):
 def get_namespace_blockchain_record(id):
     try:
         blockchain_record = bs_client.get_namespace_blockchain_record(id)
-        return jsonify(blockchain_record), 200
+        if blockchain_record == {}:
+            return jsonify(blockchain_record), 404, {'Cache-Control': 'public, max-age=300'}
+        else:
+            return jsonify(blockchain_record), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -105,7 +114,7 @@ def get_num_names_in_namespace(id):
     try:
         reply = {}
         reply["count"] = bs_client.get_num_names_in_namespace(id)
-        return jsonify(reply), 200
+        return jsonify(reply), 200, {'Cache-Control': 'public, max-age=300'}
     except Exception as e:
         return jsonify(str(e)), 500
 
@@ -114,36 +123,9 @@ def get_num_names_in_namespace(id):
 def ping():
     try:
         result = bs_client.getinfo()
-        return jsonify(result), 200
+        return jsonify(result), 200, {'Cache-Control': 'public, max-age=60'}
     except Exception as e:
         return jsonify(str(e)), 500
-
-@app.route('/get_latest_nameops/<number>', methods=['GET'])
-@crossdomain(origin='*')
-def get_latest_nameops(number):
-    try:
-        result = []
-        info = bs_client.getinfo()
-        height = int(info["last_block_processed"])
-        # from FIRST_BLOCK_MAINNET in blockstack/lib/config.py
-        BLOCKSTACK_GENESIS_BLOCK = 373601;
-
-
-        while height >= BLOCKSTACK_GENESIS_BLOCK and len(result) < number:
-            nameops = bs_client.get_nameops_at(height)
-            print nameops
-            print height
-            i = 0
-            while i < len(nameops) and len(result) < number:
-                result.append(nameops[i])
-                i = i + 1
-            height = height - 1
-
-
-        return jsonify(result), 200
-    except Exception as e:
-        return jsonify(str(e)), 500
-
 
 def runserver():
     port = int(os.environ.get('PORT', 5000))

abbe574940718d2af35cab19f049afc8ef292a56
Ken <yukanliao@gmail.com>
2017-09-26 22:55:49 +0000

    Add documentation for prod docker setup. #45

--- tools/deployment/start_blockstack.sh
+++ tools/deployment/start_blockstack.sh
@@ -3,5 +3,5 @@
 # blockstack-server won't start if bitcoind isn't ready to accept connections
 # wait for 30 seconds to make give bitcoind time to start up on boot 
 sleep 30
-/data/blockstack-venv/bin/python2.7 /data/blockstack-venv/bin/blockstack-server stop
-/data/blockstack-venv/bin/python2.7 /data/blockstack-venv/bin/blockstack-server start --foreground
+blockstack api stop
+blockstack api start

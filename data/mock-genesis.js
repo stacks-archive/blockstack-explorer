1acccae791d1c87a633b7e820a49e9515e9f511d
Hank Stoever <hstove@gmail.com>
2018-07-09 14:05:42 -0700

    random genesis block, closes #6

--- data/mock-genesis.js
+++ data/mock-genesis.js
@@ -1,19 +1,36 @@
+const randomAddress = () => {
+  return Math.random().toString(36).substring(24);
+}
+
+const rows = [];
+const count = 10;
+
+for (let index = 0; index < count; index++) {
+  const total = Math.round((Math.random() * 1000))
+  const vestingTotal = Math.round((Math.random() * 1000));
+  let vestedCount = vestingTotal;
+  const vestingPeriods = Math.round((Math.random() * 12));
+  const vesting = {};
+  for (let period = 1; period < vestingPeriods; period++) {
+    const periodAmount = Math.round((Math.random() * vestedCount));
+    vestedCount -= periodAmount;
+    vesting[period * 25] = periodAmount;
+  }
+
+  vesting[(vestingPeriods) * 25] = vestedCount;
+
+  rows.push({
+    address: randomAddress(),
+    type: 'STACKS',
+    value: total,
+    vesting: vesting,
+    vestingTotal: vestingTotal,
+  });
+}
+
 module.exports = {
   history: [
 
   ],
-  rows: [
-    {
-      address: 'asdf',
-      type: 'STACKS',
-      value: 100,
-      vesting: {
-        25: 40,
-        50: 50,
-        75: 60,
-        100: 70
-      },
-      vesting_total: 220,
-    }
-  ]
+  rows: rows,
 }

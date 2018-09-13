ffbf0c96debb9c23f17e776f33d9c900b8e5fa8f
Hank Stoever <hstove@gmail.com>
2018-09-13 14:43:50 -0700

    new genesis, better times

--- scripts/generate-csv.js
+++ scripts/generate-csv.js
@@ -1,17 +1,19 @@
 const fs = require('fs-extra');
 const papaparse = require('papaparse');
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 const { blockToTime } = require('../lib/addresses');
 
 const run = async () => {
   let accounts = await fs.readJson('./data/genesis.json');
-  accounts = accounts.map((account) => ({
-    lock_send_date: moment(blockToTime(account.lock_send))
-      .utcOffset('-05:00')
-      .format(),
-    ...account,
-  }));
+  accounts = accounts.map((account) => {
+    const date = moment(blockToTime(account.lock_send)).utc(false);
+    return {
+      lock_send_datetime: date.format('MM/DD/YYYY hh:mm a z'),
+      lock_send_date: date.format('MM/DD/YYYY'),
+      ...account,
+    };
+  });
   const csv = papaparse.unparse(accounts);
   await fs.writeFile('./data/genesis.csv', csv);
 };

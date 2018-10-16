201c3caa3facebb0cc0163d76a48c08bf73bf243
Hank Stoever <hstove@gmail.com>
2018-10-15 21:07:21 -0700

    package updates

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Router from 'next/router';
 import { connect } from 'react-redux';
 
-import { Flex, Box } from 'blockstack-ui';
+import { Flex, Box } from 'grid-styled';
 
 import Head from '@components/head';
 import Nav from '@components/nav';
@@ -15,22 +15,22 @@ import { Input } from '@styled/input';
 import { Type } from '@styled/typography';
 
 class Home extends React.Component {
-// const Home = () => (
+  // const Home = () => (
 
   state = {
-    address: "",
+    address: '',
   };
 
   onEnter(event) {
     if (event.key === 'Enter') {
-      this.submit()
+      this.submit();
     }
   }
 
   onChange(event) {
     this.setState({
-      address: event.target.value
-    })
+      address: event.target.value,
+    });
   }
 
   submit() {
@@ -49,42 +49,44 @@ class Home extends React.Component {
               <Card textAlign="center" p={10}>
                 <Card.HeaderBig>
                   <Type.h2 my={3} fontWeight={500} color="#fff">
-                    Welcome to the block explorer for <br/>the draft Stacks Genesis Block.
+                    Welcome to the block explorer for <br />
+                    the draft Stacks Genesis Block.
                   </Type.h2>
                 </Card.HeaderBig>
                 <Card.Content>
                   <Type.p fontSize={3} my={3}>
                     Enter your Stacks address below to look up your allocation
                   </Type.p>
-                  <Input my={3} 
-                    value={this.state.address} 
-                    placeholder="eg. SPNN289GPP5HQA5ZF2FKQKJM3K2MPNPDD6QYA2J5" 
-                    autoFocus 
-                    onKeyUp={(evt) => this.onEnter(evt)} 
-                    onChange={(evt) => this.setState({ address: evt.target.value })} 
-                    width="66%" />
+                  <Input
+                    my={3}
+                    value={this.state.address}
+                    placeholder="eg. SPNN289GPP5HQA5ZF2FKQKJM3K2MPNPDD6QYA2J5"
+                    autoFocus
+                    onKeyUp={(evt) => this.onEnter(evt)}
+                    onChange={(evt) => this.setState({ address: evt.target.value })}
+                    width="66%"
+                  />
                   <Button onClick={() => this.submit()}>Submit</Button>
                   <Type.p mt={3} mb={1} fontSize={1}>
                     If you submitted your wallet through CoinList, please make sure your public address
-                    <br/>
+                    <br />
                     matches the one listed in your CoinList account under “Purchases” and then “Distributions.”
                   </Type.p>
                   <Type.p mt={1} mb={3} fontSize={1}>
-                    New versions of the genesis block are published roughly every 24 hours. 
-                    <br/>
+                    New versions of the genesis block are published roughly every 24 hours.
+                    <br />
                     If you recently submitted an address or edits, please check back tomorrow.
                   </Type.p>
                 </Card.Content>
               </Card>
             </Box>
           </Flex>
-          <Wrap.Push></Wrap.Push>
+          <Wrap.Push />
         </Wrap.Inner>
         <Footer />
       </Wrap>
-    )
+    );
   }
-
 }
 
 export default Home;
---
+++ styled/list.js
@@ -0,0 +1,8 @@
+import Styled from 'styled-components';
+
+export const Section = Styled.section`
+  border: 1px solid #C4D8E5;
+  border-radius: 6px;
+  background: white;
+  box-shadow: 0 0 2em rgba(0, 0, 0, 0.1);
+`

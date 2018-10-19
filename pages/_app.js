3af840ef66abfa56822a18451ba18fab2ecd2390
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-19 14:45:48 -0500

    use createGlobalStyle and bump to styled-components-v4

--- pages/_app.js
+++ pages/_app.js
@@ -6,17 +6,55 @@ import { ThemeProvider } from 'styled-components';
 import withReduxStore from '../lib/with-redux-store';
 import { theme } from 'blockstack-ui';
 
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'polished';
+import fonts from '../lib/fonts';
+
+/**
+ * Reset our styles
+ */
+const Global = createGlobalStyle`
+${normalize()};
+* {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+}
+@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,400,500,Roboto:400,700');
+@font-face {
+  font-family: "Futura Heavy";
+  src: url(/static/assets/fonts/futura-heavy.ttf);
+}
+body, html{
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  background: #F1F6F9;
+  height: 100%;
+  margin: 0;
+}
+#__next{
+  height:100%;
+  margin:0;
+}
+a {
+  text-decoration: none;
+}
+${fonts}
+`;
+
 class MyApp extends App {
   render() {
     const { Component, pageProps, reduxStore } = this.props;
 
     return (
       <Container>
-        <Provider store={reduxStore}>
-          <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </Provider>
+        <>
+          <Global />
+          <Provider store={reduxStore}>
+            <ThemeProvider theme={theme}>
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </Provider>
+        </>
       </Container>
     );
   }
--- pages/_document.js
+++ pages/_document.js
@@ -1,45 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import { injectGlobal, ServerStyleSheet } from 'styled-components';
-import { normalize } from 'polished';
-
-import fonts from '../lib/fonts';
-
-/**
- * Reset our styles
- */
-injectGlobal`
-${normalize()};
-* {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  box-sizing: border-box;
-}
-@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,400,500,Roboto:400,700');
-@font-face {
-  font-family: "Futura Heavy";
-  src: url(/static/assets/fonts/futura-heavy.ttf);
-}
-body, html{
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  background: #F1F6F9;
-  height: 100%;
-  margin: 0;
-}
-#__next{
-  height:100%;
-  margin:0;
-}
-a {
-  text-decoration: none;
-}
-// h1, h2, h3, h4, h5, h6{
-//   font-family: 'Plex';
-//   margin: 0;
-//   padding: 0;
-// }
-${fonts}
-`;
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {

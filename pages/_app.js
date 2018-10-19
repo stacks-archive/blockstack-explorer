0db0322bc355423922d381480c9d2b65a5597107
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-19 14:44:18 -0500

    Add theme from blockstack-ui

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { withRouter } from 'next/router';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import withReduxStore from '../lib/with-redux-store';
+import { theme } from 'blockstack-ui';
 
 class MyApp extends App {
   render() {
@@ -12,7 +13,7 @@ class MyApp extends App {
     return (
       <Container>
         <Provider store={reduxStore}>
-          <ThemeProvider theme={{}}>
+          <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
         </Provider>

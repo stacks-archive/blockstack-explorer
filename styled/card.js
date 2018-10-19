42c89bb0ad0090f54154b35d7d089de5b137153c
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-19 15:13:03 -0500

    use card from blockstack-ui

--- styled/card.js
+++ styled/card.js
@@ -1,29 +1,23 @@
-import { Card as RebassCard } from 'rebass';
-import Styled from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
+import { Card, Box } from 'blockstack-ui';
 
-const Card = RebassCard.extend`
-  padding: 0;
-  text-align: ${({ textAlign }) => textAlign || 'left'};
-  box-shadow: 0 0 3px #aaa;
-  box-radius: 3px;
+Card.HeaderBig = styled(Box)`
+  margin: 0;
+  padding: 30px 60px;
+  background-color: rgb(32, 12, 43);
+  color: #fff;
 `;
 
-Card.HeaderBig = Styled.div`
-	margin:0;
-	padding:30px 60px;
-	background-color: rgb(32,12,43);
-	color: #fff;
+Card.Header = styled(Box)`
+  margin: 0;
+  padding: 20px 40px;
+  background-color: rgb(32, 12, 43);
+  color: #fff;
 `;
 
-Card.Header = Styled.div`
-	margin:0;
-	padding:20px 40px;
-	background-color: rgb(32,12,43);
-	color: #fff;
-`;
-
-Card.Content = Styled.div`
-	padding: 40px;
+Card.Content = styled(Box)`
+  padding: 40px;
 `;
 
 export default Card;

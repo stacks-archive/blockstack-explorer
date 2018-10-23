c82c762ced526dc502773c506b533c24b3ad6e09
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-23 14:06:37 -0500

    comments

--- components/user/index.js
+++ components/user/index.js
@@ -5,6 +5,9 @@ import { Social } from '@components/social';
 import { ConnectedAvatar } from '@components/avatar';
 import { ConnectedAppsList } from '@components/apps';
 
+/**
+ * Misc visual components
+ */
 const SubSection = ({ label, children, ...rest }) => (
   <Box pt={4} {...rest}>
     <Box pb={2}>
@@ -13,15 +16,16 @@ const SubSection = ({ label, children, ...rest }) => (
     {children}
   </Box>
 );
-
 const SectionLabel = ({ ...rest }) => (
   <Type textTransform="uppercase" letterSpacing={1.25} fontWeight="bold" color="#87acc4" fontSize={'11px'} {...rest} />
 );
-
 const Section = ({ ...rest }) => (
   <Flex color="blue.dark" flexDirection="column" px={4} borderBottom="1px solid" borderColor="blue.mid" {...rest} />
 );
 
+/**
+ * Keys we want to display
+ */
 const blockKeys = [
   'block_number',
   'first_registered',
@@ -29,7 +33,6 @@ const blockKeys = [
   'namespace_block_number',
   'preorder_block_number',
 ];
-
 const keys = [
   'importer',
   'importer_address',
@@ -43,6 +46,9 @@ const keys = [
   'value_hash',
 ];
 
+/**
+ * Pass an object and set of keys and this will render subsections for each
+ */
 const generateAutomaticSubSections = (data, arr = keys) => {
   return arr.map(
     (key, i) =>
@@ -56,12 +62,20 @@ const generateAutomaticSubSections = (data, arr = keys) => {
   );
 };
 
+/**
+ * Some latest transaction data
+ *
+ * Currently hidden because it might not be helpful
+ */
 const TransactionsSection = ({ nameRecord, ...rest }) => (
   <Section pb={4} {...rest}>
     {generateAutomaticSubSections(nameRecord)}
   </Section>
 );
 
+/**
+ * Most recent block related keys
+ */
 const BlocksSection = ({ nameRecord, ...rest }) => (
   <Section pb={4} pr={4} {...rest}>
     <Box display="grid" gridTemplateColumns="40% 40%">
@@ -70,6 +84,11 @@ const BlocksSection = ({ nameRecord, ...rest }) => (
   </Section>
 );
 
+/**
+ * Apps
+ *
+ * if the user has multiplayer apps and they are on app.co, we can display them!
+ */
 const AppsSection = ({ apps, ...rest }) =>
   apps ? (
     <ConnectedAppsList
@@ -82,6 +101,9 @@ const AppsSection = ({ apps, ...rest }) =>
     />
   ) : null;
 
+/**
+ * ID and Owner address
+ */
 const ProfileSection = ({ id, ownerAddress, ...rest }) => (
   <Section pb={4} {...rest}>
     <SubSection label="Blockstack ID">
@@ -93,6 +115,9 @@ const ProfileSection = ({ id, ownerAddress, ...rest }) => (
   </Section>
 );
 
+/**
+ * The 'profile' like part of the card, displays avatar, name, desc, and social proofs
+ */
 const IdentitySection = ({ name, id, description, account, ...rest }) => (
   <Section py={6} textAlign="center" justifyContent="center" alignItems="center">
     <ConnectedAvatar mb={5} />
@@ -113,6 +138,9 @@ const IdentitySection = ({ name, id, description, account, ...rest }) => (
   </Section>
 );
 
+/**
+ * Footer: view raw zone file button
+ */
 const ViewZoneFileSection = ({ zoneFileUrl, ...rest }) => (
   <Section py={4} {...rest}>
     <Button is="a" href={zoneFileUrl} target={'_blank'}>
@@ -121,6 +149,9 @@ const ViewZoneFileSection = ({ zoneFileUrl, ...rest }) => (
   </Section>
 );
 
+/**
+ * Bring it all together now
+ */
 const UserCard = ({ nameRecord, profile, zone_file, id, owner_address, ...rest }) => {
   if (!profile) return null;
   const { target: zone_file_url } = zone_file.uri[0];

88af7453dbfd58977d08fc5acde6e9e95ba6caf1
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-23 16:33:47 -0500

    pass variable to client

---
+++ .babelrc.js
@@ -0,0 +1,27 @@
+const env = require('./env-config.js');
+
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    ['transform-define', env],
+    'babel-plugin-styled-components',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@components': './components',
+          '@containers': './containers',
+          '@stores': './stores',
+          '@pages': './pages',
+          '@common': './common',
+          '@utils': './utils',
+          '@client': './lib/client',
+          '@styled': './styled',
+        },
+      },
+    ],
+    '@babel/plugin-transform-regenerator',
+    '@babel/plugin-transform-runtime',
+  ],
+};
---
+++ env-config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'process.env.BASE_URL': process.env.BASE_URL || 'http://localhost:3000',
+};

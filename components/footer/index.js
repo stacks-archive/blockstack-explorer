809bd617bfccff39a15302b84dee615ce90ab128
Hank Stoever <hstove@gmail.com>
2018-10-24 22:00:20 -0700

    footer legal copy

--- components/footer/index.js
+++ components/footer/index.js
@@ -5,16 +5,19 @@ import { BlockstackOutlined } from '@components/svg';
 const Footer = ({ ...rest }) => (
   <Box bg="blue.dark" color="white" px={5} py={6} {...rest}>
     <Box pb={4}>
-      <Type color={'blue.mid'} fontWeight={500}>
+      <Type color="blue.mid" fontWeight={500}>
         Blockstack Token LLC
       </Type>
     </Box>
     <Box>
-      <Type color={'blue.mid'}>
-        For information related to the Stacks token, please visit{' '}
-        <Type is="a" href={'https://stackstoken.com'} target={'_blank'} color={'white'}>
-          stackstoken.com
+      <Type color="blue.mid" fontSize={1}>
+        The Blockstack Tokens are a crypto asset that is currently being developed by Blockstack Token LLC, a Delaware
+        {'limited liability company, whose website can be found at '}
+        <Type is="a" href="https://stackstoken.com" target="_blank" color="white">
+          www.blockstack.com
         </Type>
+        . The website you are currently visiting (explorer.blockstack.org) is sponsored by Blockstack PBC, an affiliate
+        of Blockstack Token LLC, and should not be viewed as an offer or sale of securities.
       </Type>
     </Box>
   </Box>

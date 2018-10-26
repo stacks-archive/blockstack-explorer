219fd56fe9846ef2d22dc8b37a744e64fd8c6c38
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-26 08:38:38 -0500

    footer adjustments #29

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,14 +3,12 @@ import { Box, Flex, Type } from 'blockstack-ui';
 import { BlockstackOutlined } from '@components/svg';
 
 const Footer = ({ ...rest }) => (
-  <Box bg="blue.dark" color="white" px={5} py={6} {...rest}>
-    <Box pb={4}>
-      <Type color="blue.mid" fontWeight={500}>
+  <Box bg="blue.dark" color="white" px={4} py={7} {...rest}>
+    <Flex flexDirection="column" px={5} color="white" opacity={0.5}>
+      <Type pb={4} fontWeight={500}>
         Blockstack Token LLC
       </Type>
-    </Box>
-    <Box>
-      <Type lineHeight={1.7} color="blue.mid" maxWidth="900px" fontSize={'12px'}>
+      <Type lineHeight={1.7} maxWidth="900px" fontSize={'12px'}>
         The Blockstack Tokens are a crypto asset that is currently being developed by Blockstack Token LLC, a Delaware
         {'limited liability company, whose website can be found at '}
         <Type is="a" href="https://stackstoken.com" target="_blank" color="white">
@@ -19,7 +17,7 @@ const Footer = ({ ...rest }) => (
         . The website you are currently visiting (explorer.blockstack.org) is sponsored by Blockstack PBC, an affiliate
         of Blockstack Token LLC, and should not be viewed as an offer or sale of securities.
       </Type>
-    </Box>
+    </Flex>
   </Box>
 );
 

e6fed69267f65ec0dd2e0904ec7af03575ce57d6
Hank Stoever <hstove@gmail.com>
2018-10-25 23:55:29 -0700

    transaction details, fixes #32 #46

--- containers/cards/transaction.js
+++ containers/cards/transaction.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Box, Type } from 'blockstack-ui';
 import { Card } from '@components/card/index';
 import { Section } from '@components/section/index';
+import { Attribute } from '@components/attribute';
+import { Time } from '@components/time';
 
 import Link from 'next/link';
 
@@ -27,7 +29,7 @@ const generateAutomaticSections = (data, arr, params) => {
     (key, i) =>
       data[key] ? (
         <Section.Subsection label={key} key={i}>
-          <Box maxWidth={'100%'} overflow="auto">
+          <Box maxWidth="100%" overflow="auto">
             {params ? (
               <LinkWrapper query={data[key]}>
                 <Type is="a" fontFamily="brand">
@@ -43,14 +45,32 @@ const generateAutomaticSections = (data, arr, params) => {
   );
 };
 
+const keys = ['size', 'txid', 'blockhash'];
+
 /**
  * Some latest transaction data
  *
  * Currently hidden because it might not be helpful
  */
-const AutomatedSection = ({ iterable, ...rest }) => (
+const AutomatedSection = ({ tx, ...rest }) => (
   <Section pb={4} {...rest}>
-    {generateAutomaticSections(iterable, Object.keys(iterable))}
+    <Attribute
+      label="Block"
+      value={tx.blockheight}
+      link={{
+        href: {
+          pathname: '/blocks/single',
+          query: {
+            hash: tx.blockhash,
+          },
+        },
+        as: `/block/${tx.blockhash}`,
+      }}
+    />
+    <Attribute label="time">
+      <Time date={tx.time} tooltip={false} />
+    </Attribute>
+    {generateAutomaticSections(tx, keys)}
   </Section>
 );
 
@@ -58,10 +78,10 @@ const AutomatedSection = ({ iterable, ...rest }) => (
  * Bring it all together now
  */
 const TransactionCard = ({ transaction, ...rest }) => {
-  const { vin, vout, id, ...iterable } = transaction;
+  const { vin, vout, id, ...tx } = transaction;
   return (
-    <Card title={`Transaction Information`} {...rest}>
-      <AutomatedSection iterable={iterable} />
+    <Card title="Transaction Information" {...rest}>
+      <AutomatedSection tx={tx} />
     </Card>
   );
 };

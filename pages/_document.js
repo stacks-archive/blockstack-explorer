07f21ade7c2c4226d74ecb46bd3ce65c50a594e9
Hank Stoever <hstove@gmail.com>
2018-10-30 11:48:02 -0700

    google analytics, fixes #64

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+const GoogleCode = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-119163063-3');
+`;
+
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
@@ -13,7 +20,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119163063-3" />
+          <script dangerouslySetInnerHTML={{ __html: GoogleCode }} />
+        </Head>
         <body>
           <Main />
           <NextScript />

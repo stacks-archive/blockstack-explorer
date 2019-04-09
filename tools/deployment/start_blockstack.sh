d5f03e30c63043723c83d1acc1d43209765485aa
Aaron Blankstein <aaron@blockstack.com>
2019-04-09 13:39:39 -0500

    clear current files

--- tools/deployment/start_blockstack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# blockstack-server won't start if bitcoind isn't ready to accept connections
-# wait for 30 seconds to make give bitcoind time to start up on boot 
-sleep 30
-blockstack api stop
-blockstack api start

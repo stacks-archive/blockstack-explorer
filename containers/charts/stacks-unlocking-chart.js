66ff84b50e462d8dae4b05e308fd3913be9863c3
Hank Stoever <hstove@gmail.com>
2019-04-23 11:02:35 -0700

    consistent casing on stx address page, fixes #102

--- containers/charts/stacks-unlocking-chart.js
+++ containers/charts/stacks-unlocking-chart.js
@@ -77,7 +77,7 @@ const StacksUnlockingChart = ({ ...rest }) => (
             };
 
             return (
-              <Card title="STX unlocking schedule" {...rest}>
+              <Card title="STX Unlocking Schedule" {...rest}>
                 <Section py={6}>
                   <Type mb={6} fontWeight="normal">
                     {`This address will start unlocking for transfer on `}
@@ -102,12 +102,14 @@ const StacksUnlockingChart = ({ ...rest }) => (
                       blockchain.
                     </Disclaimer>
                     <Disclaimer>
-                      <strong>Disclaimer:</strong> The data here are based on prices for the tokens as inputted by users
+                      <strong>Disclaimer:</strong>
+{' '}
+The data here are based on prices for the tokens as inputted by users
                       into this projection tool and may not reflect their current pricing or any market data, which may
                       be higher or lower than what is shown here. There is no guarantee that the market cap shown will
                       be in existence at any particular time, or that Stacks Tokens can be sold at a price that is more
                       advantageous or disadvantageous for a particular buyer at any particular point in time.
-                    </Disclaimer>
+</Disclaimer>
                     <Disclaimer>
                       No graph, chart, or formula can in and of itself determine which securities or other investments
                       to buy or sell or when to buy or sell them. Potential investors should not rely on this or any

f62ec7879149316211790f18e21be9899afa5ef5
Hank Stoever <hstove@gmail.com>
2019-05-29 16:33:43 -0700

    update next.js to 8

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const webpackConfig = require('./webpack.config');
 
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-    config.resolve = webpackConfig.resolve;
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      ...webpackConfig.resolve.alias,
+    };
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|svg)$/,
       loader: 'url-loader?limit=100000',

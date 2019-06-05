c668861f751adc50251eae123358301b6bea3bc9
Hank Stoever <hstove@gmail.com>
2019-06-05 14:16:28 -0700

    no more pagination on name operations list, fixes #76

--- pages/names/single.js
+++ pages/names/single.js
@@ -37,88 +37,41 @@ class NamesSinglePage extends React.Component {
     };
   }
 
-  state = {
-    pageNum: 0,
-    data:
-      this.props.user.nameRecord && this.props.user.nameRecord.history ? [...this.props.user.nameRecord.history] : [],
-    limit: 5,
-    loading: false,
-    doesNotHaveNextPage: false,
-  };
+  // state = {
+  //   pageNum: 0,
+  //   data:
+  //     this.props.user.nameRecord && this.props.user.nameRecord.history ? [...this.props.user.nameRecord.history] : [],
+  //   limit: 5,
+  //   loading: false,
+  //   doesNotHaveNextPage: false,
+  // };
 
   componentDidMount() {
-    this.preloadNextPage();
+    // this.preloadNextPage();
   }
 
-  componentDidUpdate(prevProps, prevState, prevContext) {
-    const { user } = this.props;
-    if (prevProps.user.id !== user.id) {
-      // different user, reset to initial state
-      this.setState(
-        {
-          pageNum: 0,
-          data: user.nameRecord && user.nameRecord.history ? [...user.nameRecord.history] : [],
-          limit: 5,
-          loading: false,
-          doesNotHaveNextPage: false,
-        },
-        () => this.preloadNextPage(),
-      );
-    }
-  }
-
-  showMoreItems = () => {
-    const { data, limit } = this.state;
-    const all = data.length;
-    if (limit < all) {
-      if (limit + 5 > all) {
-        this.setState(() => ({
-          limit: all,
-        }));
-        this.preloadNextPage();
-      } else {
-        this.setState((state) => ({
-          limit: state.limit + 5,
-        }));
-      }
-    }
-  };
-
-  // eslint-disable-next-line react/destructuring-assignment
-  preloadNextPage = async (page = this.state.pageNum + 1) => {
-    const { data } = this.state;
-    const { user } = this.props;
-    if (data.length >= 20) {
-      const { id } = user;
-      this.setState({
-        loading: true,
-      });
-      try {
-        const newData = await fetchName(id, page);
-        if (newData && newData.nameRecord && newData.nameRecord.history && newData.nameRecord.history.length) {
-          this.setState((state) => ({
-            data: [...state.data, ...newData.nameRecord.history],
-            loading: false,
-          }));
-        }
-      } catch (e) {
-        // if we err, eg 404, set doesNotHaveNextPage true
-        this.setState({
-          loading: false,
-          doesNotHaveNextPage: true,
-        });
-      }
-    }
-  };
+  // componentDidUpdate(prevProps, prevState, prevContext) {
+  //   const { user } = this.props;
+  //   if (prevProps.user.id !== user.id) {
+  //     // different user, reset to initial state
+  //     this.setState(
+  //       {
+  //         pageNum: 0,
+  //         data: user.nameRecord && user.nameRecord.history ? [...user.nameRecord.history] : [],
+  //         limit: 5,
+  //         loading: false,
+  //         doesNotHaveNextPage: false,
+  //       },
+  //       () => this.preloadNextPage(),
+  //     );
+  //   }
+  // }
 
   render() {
     const { user } = this.props;
-    const { data: allItems, limit, doesNotHaveNextPage, loading } = this.state;
     const nameExists = user;
     if (!nameExists) return <Empty />;
 
-    const items = allItems.slice(0, limit);
-    const showMore = allItems.length > limit;
     if (!user.nameRecord) {
       return (
         <Page key={user.id}>
@@ -140,13 +93,7 @@ class NamesSinglePage extends React.Component {
         <UserCard mb={[5, 5, 0]} mr={[0, 0, 5]} width={1} maxWidth={['100%', '100%', '380px']} {...user} />
         <Page.Main>
           <Card flexGrow={1} title="Recent Operations">
-            <NameOperationsList key={user.id} items={items} />
-            {showMore &&
-              !doesNotHaveNextPage && (
-                <Flex py={4} justifyContent="center">
-                  <Button onClick={() => this.showMoreItems()}>{loading ? 'Loading...' : 'View More'}</Button>
-                </Flex>
-              )}
+            <NameOperationsList key={user.id} items={user.nameRecord} />
           </Card>
         </Page.Main>
       </Page>

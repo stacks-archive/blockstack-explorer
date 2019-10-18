90a671d3c9997e326060c1fcf1b2af09c9bd738e
Hank Stoever <hstove@gmail.com>
2019-10-18 11:31:03 -0700

    cache keys use current git tag

--- server/controllers/app-controller.js
+++ server/controllers/app-controller.js
@@ -2,17 +2,30 @@ const express = require('express');
 
 const dev = process.env.NODE_ENV !== 'production';
 
+const getCurrentGitTag = () =>
+  new Promise(async (resolve) => {
+    const command = "git describe --exact-match --tags $(git log -n1 --pretty='%h')";
+    // eslint-disable-next-line global-require
+    require('child_process').exec(command, (err, stdout) => {
+      if (err) {
+        return resolve('');
+      }
+      return resolve(stdout);
+    });
+  });
+
 /*
  * NB: make sure to modify this to take into account anything that should trigger
  * an immediate page change (e.g a locale stored in req.session)
  */
-function getCacheKey(req) {
-  return `${req.url}`;
+async function getCacheKey(req) {
+  const tag = await getCurrentGitTag();
+  return `${req.url}-${tag}`;
 }
 
 const makeAppController = (app, cache) => {
   const renderAndCache = async (req, res, pagePath) => {
-    const key = getCacheKey(req);
+    const key = await getCacheKey(req);
 
     // If we have a page in the cache, let's serve it
     if (cache.has(key) && !dev) {

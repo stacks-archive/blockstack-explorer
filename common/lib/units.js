f19b709f19e55e740dc48ab82765dd42e0421e1e
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-10-25 11:05:09 -0500

    add formatValue fn to present a cleaner large number

--- common/lib/units.js
+++ common/lib/units.js
@@ -1,3 +1,7 @@
-export const stacksValue = (value) => `${+`${Math.round(`${value * 10e-7}e+7`)}e-7`} STX`;
+import numeral from 'numeral';
 
-export const btcValue = (value) => +`${Math.round(`${value * 10e-9}e+9`)}e-9`;
+export const formatValue = (v) => numeral(parseInt(v, 10)).format('0,0[.]000000');
+
+export const stacksValue = (value) => `${formatValue(+`${Math.round(`${value * 10e-7}e+7`)}e-7`)} STX`;
+
+export const btcValue = (value) => +`${formatValue(Math.round(`${value * 10e-9}e+9`))}e-9`;

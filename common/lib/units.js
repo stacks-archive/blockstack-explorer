39c442dc39480cbebc8996e39ce3664a560ef659
Hank Stoever <hstove@gmail.com>
2019-11-06 11:50:21 -0800

    only display locked/unlocked from API server

--- common/lib/units.js
+++ common/lib/units.js
@@ -2,7 +2,7 @@ import numeral from 'numeral';
 
 export const formatValue = (v) => {
   const number = parseFloat(v);
-  return number >= 1000 ? numeral(number).format('0,0.[000000]') : number;
+  return number >= 1000 ? numeral(number).format('0,0.[000]') : number;
 };
 
 export const stacksValue = (value) => `${formatValue(+`${Math.round(`${value * 10e-7}e+7`)}e-7`)} STX`;
--- containers/cards/stacks-address.js
+++ containers/cards/stacks-address.js
@@ -2,36 +2,28 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 import { Flex, Box, Type } from 'blockstack-ui';
 import PropTypes from 'prop-types';
+import numeral from 'numeral';
 import { Card } from '@components/card';
 import { Section } from '@components/section';
 import { Attribute } from '@components/attribute';
 import { stacksValue } from '@common/lib/units';
 
-const getLockedAmount = (vestingTotal, history = [], balance) => {
+const getLockedAmount = (history = []) => {
   // const available = credit - debit;
-  let unlocked = 0;
   let sent = 0;
   let received = 0;
   history.forEach((transfer) => {
-    if (transfer.operation === 'UNLOCK') {
-      unlocked += transfer.value;
-    } else if (transfer.operation === 'SENT') {
+    if (transfer.operation === 'SENT') {
       sent += transfer.value;
     } else if (transfer.operation === 'RECEIVED') {
       received += transfer.value;
     }
   });
-  const locked = vestingTotal - unlocked;
-  // return [locked, unlocked];
-  let total = balance;
-  if (vestingTotal) {
-    total = vestingTotal - sent + received;
-  }
-  return { locked, unlocked, sent, received, total };
+  return { sent, received };
 };
 
-const StacksAddressCard = ({ address: { address, balance, status, vesting_total: vestingTotal, history } }) => {
-  const { locked, unlocked, sent, received, total } = getLockedAmount(vestingTotal, history, balance);
+const StacksAddressCard = ({ address: { address, totalUnlocked, totalLocked, balance, status, history } }) => {
+  const { sent, received } = getLockedAmount(history);
   return address ? (
     <Card width={1} title="Stacks Address Details" pb={4}>
       <Section alignItems="center" justifyContent="center" py={4} color="blue.dark">
@@ -40,16 +32,16 @@ const StacksAddressCard = ({ address: { address, balance, status, vesting_total:
       <Section pb={4} borderBottom="0">
         <Attribute label="Stacks Address" value={address} />
         <Section.Subsection label="Total at this address">
-          <Type fontSize={3}>{stacksValue(total)}</Type>
+          <Type fontSize={3}>{stacksValue(parseInt(balance, 10) + totalLocked)}</Type>
         </Section.Subsection>
         <Section.Subsection label="Cumulative Address Activity" mt={2} />
         <Flex>
           <Box width={1 / 2}>
             <Section.Subsection label="Locked">
-              <Type fontFamily="brand">{stacksValue(isNaN(locked) ? 0 : locked)}</Type>
+              <Type fontFamily="brand">{stacksValue(totalLocked)}</Type>
             </Section.Subsection>
             <Section.Subsection label="unlocked">
-              <Type fontFamily="brand">{stacksValue(unlocked)}</Type>
+              <Type fontFamily="brand">{stacksValue(totalUnlocked)}</Type>
             </Section.Subsection>
           </Box>
           <Box width={1 / 2}>

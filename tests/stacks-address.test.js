9857b7be5e7c48f9c1622dfce8bb9ad48abd9fee
Hank Stoever <hstove@gmail.com>
2019-11-08 15:04:58 -0800

    more stx address tests

--- tests/stacks-address.test.js
+++ tests/stacks-address.test.js
@@ -2,6 +2,8 @@
 const url = process.env.TESTING_URL || 'http://localhost:3000';
 jest.setTimeout(15000);
 
+const getNumber = (text) => parseFloat(text.split(' ')[0].replace(',', ''));
+
 describe('Stacks address page', () => {
   describe('address SP1ERE1Z1MDVM100NWYR16GZZJJTZQYHG4F6GWBDM', () => {
     const address = 'SP1ERE1Z1MDVM100NWYR16GZZJJTZQYHG4F6GWBDM';
@@ -21,6 +23,16 @@ describe('Stacks address page', () => {
       await expect(page).toMatch('Received 2,180.654 STX');
       await expect(page).toMatch('Sent 27,714.625 STX');
       await expect(page).toMatch('Sent 100 STX');
+    });
+
+    test('locked amounts are correct', async () => {
+      const unlockedEl = await page.$eval('.subsection-unlocked', (el) => el.innerText);
+      const unlocked = getNumber(unlockedEl);
+      expect(unlocked).toBeGreaterThanOrEqual(22569.625);
+      const lockedEl = await page.$eval('.subsection-locked', (el) => el.innerText);
+      const locked = getNumber(lockedEl);
+      expect(locked).toBeLessThanOrEqual(19097.375);
+      expect(locked + unlocked).toEqual(19097.375 + 22569.625);
     });
   });
 });

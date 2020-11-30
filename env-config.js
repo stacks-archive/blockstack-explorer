614664cc7062bd8d90950fcfbc1de36192c36f70
Thomas Osmonson <aulneau@users.noreply.github.com>
2020-11-30 10:08:11 -0600

    fix: update deps

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  'process.env.API_URL': process.env.API_URL || 'https://blockstack-explorer-api.herokuapp.com',
+  'process.env.API_URL': process.env.API_URL || 'https://explorer-api.blockstack.org',
 };
